dde49ef39eb09214ce20d97b9b55efced434e06d
yeonee21 <doublek4027@gmail.com>
2021-10-11 04:50:25 +0900

    Test fragment

--- app/src/main/java/com/example/covidapp/ui/test/TestFragment.java
+++ app/src/main/java/com/example/covidapp/ui/test/TestFragment.java
@@ -15,5 +15,7 @@ public class TestFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_test, container, false);
 
+
+
     }
 }
