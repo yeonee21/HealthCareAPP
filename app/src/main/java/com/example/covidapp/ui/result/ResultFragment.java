649ae107502479cb5c370f540606c35caae74c7e
yeonee21 <doublek4027@gmail.com>
2021-10-11 06:41:09 +0900

    fragment_result.xml fragment_test.xml

--- app/src/main/java/com/example/covidapp/ui/result/ResultFragment.java
+++ app/src/main/java/com/example/covidapp/ui/result/ResultFragment.java
@@ -15,5 +15,7 @@ public class ResultFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_result, container, false);
 
+
+
     }
 }
