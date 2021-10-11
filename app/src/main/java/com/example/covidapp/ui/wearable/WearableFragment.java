1fea079820b5c19765c0adbcce8d5f593246de0c
yeonee21 <doublek4027@gmail.com>
2021-10-12 01:09:23 +0900

    WearableFragment

--- app/src/main/java/com/example/covidapp/ui/wearable/WearableFragment.java
+++ app/src/main/java/com/example/covidapp/ui/wearable/WearableFragment.java
@@ -13,7 +13,7 @@ public class WearableFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_test, container, false);
+        return inflater.inflate(R.layout.fragment_wearable, container, false);
 
     }
 }
