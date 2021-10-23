b5085fa9469fe48cb7e214d2d55dad61b268de5a
yeonee21 <doublek4027@gmail.com>
2021-10-23 10:22:19 +0900

    wearable  fragment

--- app/src/main/java/com/example/covidapp/ui/wearable/WearableFragment.java
+++ app/src/main/java/com/example/covidapp/ui/wearable/WearableFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.SeekBar;
 
 import androidx.fragment.app.Fragment;
 
@@ -13,7 +15,13 @@ public class WearableFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_wearable, container, false);
 
+        View rootView = inflater.inflate(R.layout.fragment_wearable, container, false);
+
+        SeekBar HeartRateBar = (SeekBar) rootView.findViewById(R.id.HR_bar);
+        EditText hr_condition = (EditText) rootView.findViewById(R.id.HR_condition);
+
+        return inflater.inflate(R.layout.fragment_wearable, container, false);
     }
+
 }
