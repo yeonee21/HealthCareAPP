bdf4443a2095ab43571e91eee1293be73ff1769e
yeonee21 <doublek4027@gmail.com>
2021-10-24 01:26:08 +0900

    test edit

--- app/src/main/java/com/example/covidapp/ui/test/TestFragment.java
+++ app/src/main/java/com/example/covidapp/ui/test/TestFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
 
 import androidx.fragment.app.Fragment;
 
@@ -13,6 +14,21 @@ public class TestFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup rootView =(ViewGroup) inflater.inflate(R.layout.fragment_test, container, false);
+        RadioGroup rgTemp;
+        RadioGroup rgIllness;
+        RadioGroup rgRunNose;
+        RadioGroup rgBreath;
+        RadioGroup rgExhaust;
+        RadioGroup rgCough;
+        RadioGroup rgThroat;
+        RadioGroup rgCovidexp;
+        RadioGroup rgStuffNose;
+        RadioGroup rgDiarrhea;
+        RadioGroup rgContact;
+
+
+
         return inflater.inflate(R.layout.fragment_test, container, false);
 
 
