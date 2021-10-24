6819cbba4dd3d221d4b016ad34cd06af1944d4c6
yujin3388 <wnrmrtjd3388@naver.com>
2021-10-25 02:10:55 +0900

    오픈 API 사용하니까 UI가 너무 별로기도 하고 찾아보니까 web자체를 가져오는 방법 fragment에 적용하는 게 있길래 그걸로함 중앙대책 본부의 코로나 19 제공 정보 받은거

--- app/src/main/java/com/example/covidapp/ui/login/LoginFragment.java
+++ app/src/main/java/com/example/covidapp/ui/login/LoginFragment.java
@@ -1,12 +1,5 @@
 package com.example.covidapp.ui.login;
 
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
-import androidx.fragment.app.Fragment;
-
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -20,6 +13,13 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.covidapp.R;
 
