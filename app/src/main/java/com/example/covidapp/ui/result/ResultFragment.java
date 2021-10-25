4163b7d28563d3759917a4a5f55418452fc864e2
yeonee21 <doublek4027@gmail.com>
2021-10-25 15:02:39 +0900

    result fragment 수정

--- app/src/main/java/com/example/covidapp/ui/result/ResultFragment.java
+++ app/src/main/java/com/example/covidapp/ui/result/ResultFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
@@ -12,13 +14,29 @@ import com.example.covidapp.R;
 
 public class ResultFragment extends Fragment {
     private Spinner dataSpinner;
+    private String[] dataSelect = getResources().getStringArray(R.array.data_versions);
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_result, container, false);
+        ViewGroup rootView =(ViewGroup) inflater.inflate(R.layout.fragment_result, container, false);
+        dataSpinner = rootView.findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter =new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, dataSelect);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dataSpinner.setAdapter(adapter);
+        dataSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
 
 
+
+
+        return rootView;
 
     }
 }
